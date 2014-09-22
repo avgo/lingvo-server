ac44e864baeeb4f8231e602d53aadbef285594df
Anton Goncharov <gavmail1985@yandex.ru>
2014-09-23 01:54:06 +0400

    doc-template

--- doc-template.c
+++ doc-template.c
@@ -11,6 +11,18 @@ static int doc_template_add_link(doc_template *dte,
 		const char *s1, const char *s2,
 		const char *s3, const char *s4)
 {
+	if (s1 == NULL) {
+		printf("dte_text: '%.*s'\n",
+			(int) (s4 - s3),
+			s3);
+	}
+	else {
+		printf("dte_link: '%.*s'\n",
+			(int) (s2 - s1),
+			s1);
+	}
+
+	return 1;
 }
 
 void doc_template_free(doc_template *dte)
@@ -32,48 +44,26 @@ int doc_template_open(doc_template *dte, const char *filename)
 		return -1;
 	}
 
-	const char *s1, *s2, *s3, *s4, *s_end;
+	const char *s1, *s2, *s_end;
 
 	s1 = s2 = dte->text.data;
 	s_end = dte->text.data + (dte->text.size - 1);
 
-	while (s2 != s_end) {
-		if (*s2 == '<') {
-			++s2;
-			if (s2 == s_end) {
-				if (doc_template_add_link(dte, NULL, NULL, s1, s2) == -1)
-					return -1;
-			}
-			if (*s2 == '?') {
-				--s2;
-				if (doc_template_add_link(dte, NULL, NULL, s1, s2) == -1)
-					return -1;
-				s3 = s2 + 2;
-				for (s4 = s3; ; ++s4) {
-					if (s4 == s_end) {
-						printf("error: incomplete link.\n");
-						return 1;
-					}
-					if (*s4 == '?') {
-						++s4;
-						if (s4 == s_end) {
-							printf("error: incomplete link.\n");
-							return 1;
-						}
-						if (*s4 == '>') {
-							--s4;
-							if (doc_template_add_link(dte, s3, s4, NULL, NULL) == -1)
-								return -1;
-							s1 = s2 = s4 + 2;
-							break;
-						}
-					}
-				}
-			}
-			else
-				++s2;
+	for (;;) {
+		s2 = strstr(s1, "<?");
+		if (s2 == NULL)
+			s2 = s_end;
+		if (doc_template_add_link(dte, NULL, NULL, s1, s2) == -1)
+			return -1;
+		if (s2 == s_end)
+			break;
+		s1 = s2 + 2;
+		s2 = strstr(s1, "?>");
+		if (s2 == NULL) {
+			return 1;
 		}
-		else
-			++s2;
+		if (doc_template_add_link(dte, s1, s2, NULL, NULL) == -1)
+			return -1;
+		s1 = s2 + 2;
 	}
 }
