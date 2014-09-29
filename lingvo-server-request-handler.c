8ec66aca90574e32134783e5d4d2a464ea8069b3
Anton Goncharov <gavmail1985@yandex.ru>
2014-09-29 17:24:07 +0400

    Refactoring. Move send_response...
    ... to lingvo-server-utils.

--- lingvo-server-request-handler.c
+++ lingvo-server-request-handler.c
@@ -1,9 +1,3 @@
-#include <stdarg.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdio.h>
-#include <errno.h>
-
 #include "lingvo-server-request-handler.h"
 #include "lingvo-server-utils.h"
 
@@ -39,69 +33,6 @@ static int handlers_count = sizeof(handlers) / sizeof(*handlers);
 
 
 
-
-static int make_message(char **dst_str, int *dst_str_len, int *size_alloc, const char *fmt, va_list ap)
-{
-	char *np;
-	va_list aq;
-
-
-	*size_alloc = 128;
-	*dst_str = malloc(*size_alloc);
-	if (*dst_str == NULL)
-		return -1;
-
-	for (;;) {
-		va_copy(aq, ap);
-		*dst_str_len = vsnprintf(*dst_str, *size_alloc, fmt, aq);
-		va_end(aq);
-
-		if (*dst_str_len > -1 && *dst_str_len < *size_alloc)
-			return 1;
-
-		if (*dst_str_len > -1)
-			*size_alloc = *dst_str_len + 1;
-		else
-			*size_alloc *= 2;
-
-		np = realloc(*dst_str, *size_alloc);
-		if (np == NULL) {
-			free(*dst_str);
-			return -1;
-		}
-		
-		*dst_str = np;
-	}
-}
-
-static int send_response(int sock, const char *str, ...)
-{
-	va_list ap;
-	char *data = NULL;
-	int data_len;
-	int data_alloc;
-	int ret = 1, res;
-
-	va_start(ap, str);
-	res = make_message(&data, &data_len, &data_alloc, str, ap);
-	va_end(ap);
-
-	if (res == -1) {
-		ret = -1; goto END;
-	}
-
-	if (data_len == 0)
-		return 1;
-
-	if (write(sock, data, data_len) != data_len) {
-		printf("write(): %s (%u)\n", strerror(errno), errno);
-		ret = -1; goto END;
-	}
-
-END:	if (data != NULL)
-		free(data);
-	return ret;
-}
 
 int lingvo_server_request_handler(lingvo_server_request *request, int s)
 {
--- lingvo-server-utils.c
+++ lingvo-server-utils.c
@@ -1,10 +1,48 @@
 #include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
 #include <string.h>
 
 #include "lingvo-server-utils.h"
 
 
 
+
+static int make_message(char **dst_str, int *dst_str_len, int *size_alloc, const char *fmt, va_list ap)
+{
+	char *np;
+	va_list aq;
+
+
+	*size_alloc = 128;
+	*dst_str = malloc(*size_alloc);
+	if (*dst_str == NULL)
+		return -1;
+
+	for (;;) {
+		va_copy(aq, ap);
+		*dst_str_len = vsnprintf(*dst_str, *size_alloc, fmt, aq);
+		va_end(aq);
+
+		if (*dst_str_len > -1 && *dst_str_len < *size_alloc)
+			return 1;
+
+		if (*dst_str_len > -1)
+			*size_alloc = *dst_str_len + 1;
+		else
+			*size_alloc *= 2;
+
+		np = realloc(*dst_str, *size_alloc);
+		if (np == NULL) {
+			free(*dst_str);
+			return -1;
+		}
+		
+		*dst_str = np;
+	}
+}
 
 int parameter_parse(const char *s1, const char *s1_end, const char *s2)
 {
@@ -91,4 +129,33 @@ int parameter_save(const char *p1, const char *p1_end,
 		return PAR_SAVE_SYSERR;
 
 	return PAR_SAVE_MATCH;
+}
+
+int send_response(int sock, const char *str, ...)
+{
+	va_list ap;
+	char *data = NULL;
+	int data_len;
+	int data_alloc;
+	int ret = 1, res;
+
+	va_start(ap, str);
+	res = make_message(&data, &data_len, &data_alloc, str, ap);
+	va_end(ap);
+
+	if (res == -1) {
+		ret = -1; goto END;
+	}
+
+	if (data_len == 0)
+		return 1;
+
+	if (write(sock, data, data_len) != data_len) {
+		printf("write(): %s (%u)\n", strerror(errno), errno);
+		ret = -1; goto END;
+	}
+
+END:	if (data != NULL)
+		free(data);
+	return ret;
 }
