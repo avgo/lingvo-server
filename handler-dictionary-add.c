8685789da988f732c97b5d2ad2de5c21d23af4e6
Anton Goncharov <gavmail1985@yandex.ru>
2014-10-30 20:58:56 +0400

    Handler dictionary add: Using dict_maria_db.
    Adding words to dictionary with dictionary
    interface.

--- handler-dictionary-add.c
+++ handler-dictionary-add.c
@@ -1,4 +1,7 @@
+#include <lingvo/dict_maria_db.h>
+
 #include <stdio.h>
+#include <string.h>
 
 #include "lingvo-server-request.h"
 #include "lingvo-server-utils.h"
@@ -8,11 +11,37 @@
 
 int handler_dictionary_add(lingvo_server_request *request, int s)
 {
-	send_response(s, "hello");
+	int ret = 1;
+	int result;
+	lingvo_dictionary dict;
+	char *word = NULL;
+	char *message = NULL;
 
-	printf("'%.*s'\n",
-		request->content_length,
-		request->terminator);
 
-	return 1;
+	lingvo_dict_maria_db_init(&dict);
+
+
+	lingvo_dictionary_create(&dict);
+	word = strndup(request->terminator, request->content_length);
+	if (word == NULL) {
+		ret = -1; goto END;
+	}
+
+	result = lingvo_dictionary_add_word(&dict, word);
+	if (result > -1) {
+		message = "ok";
+	}
+	else {
+		message = "fail";
+	}
+
+	if (send_response(s, message) == -1) {
+		ret = -1; goto END;
+	}
+
+END:	if (word != NULL)
+		free(word);
+	lingvo_dictionary_close(&dict);
+
+	return ret;
 }
